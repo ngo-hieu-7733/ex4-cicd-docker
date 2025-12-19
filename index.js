b5a7d0d5d0cea6adfdfbbe528e3bc9c6c0346c4c
ngo-hieu-7733 <ngohieu815@gmail.com>
2025-12-19 10:40:34 +0700

    change index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 app.get('/', async (req, res) => {
-	res.status(200).send('Xin chào bạn');
+	res.status(200).send('Hello world');
 });
+
 // Start server
 app.listen(3000, () => {
 	console.log('Server running on http://localhost:3000');
